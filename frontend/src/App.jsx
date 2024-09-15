f11f867ca077cb69c90d47878f58b35d23b38330
Niraj Kumar <nk659192@gmail.com>
2024-09-15 07:39:56 +0530

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button className='p-2 bg-red-600'>click</button>
+      
+    </div>
+    
+  )
+}
+
+export default App
